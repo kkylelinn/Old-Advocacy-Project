280318d1369b4a663f89ef3bbd4c5a0ce0e3dc4c
kkylelinn <kylelin645@gmail.com>
2024-07-19 19:46:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let themebutton = document.getElementById("theme-button");
+const toggleDarkMode = (  document.body.classList.toggle ("dark-mode");
+
+themeButton.addEventListener("click", toggleDarkMode);
